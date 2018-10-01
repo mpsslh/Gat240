fd0997e415d0104a0be580ab53411a2274b13afb
mpsslh <mpsslh@hotmail.com>
2018-10-01 09:02:14 +0800

    how to switch bgm color?

---
+++ A2/Assignment2 - Waldorf Project/Assets/Scripts/CameraControllers/FollowCamera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : GameCamera
+{
+	public GameObject starting;
+	public Vector3 offset;
+	public GameObject waldorf;
+	int state;
+
+	// Use this for initialization
+	void Start()
+	{
+		state = 0;
+		gameObject.transform.position = starting.transform.position;
+		gameObject.transform.rotation = starting.transform.rotation;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonUp(0))
+		{
+			Debug.Log("left clicked");
+			state = 1;
+
+		}
+		if (Input.GetMouseButtonUp(1))
+		{
+			Debug.Log("right clicked");
+			state = 2;
+		}
+		switch (state)
+		{
+			case 1:
+				gameObject.transform.position = waldorf.GetComponent<Transform>().position;
+				gameObject.transform.rotation = waldorf.GetComponent<Transform>().rotation;
+				break;
+			case 2:
+				Debug.Log(waldorf.GetComponent<Transform>().forward);
+				gameObject.transform.position = waldorf.GetComponent<Transform>().position + waldorf.GetComponent<Transform>().forward * offset.z
+					+ waldorf.GetComponent<Transform>().right * offset.x + waldorf.GetComponent<Transform>().up * offset.y;
+				gameObject.transform.rotation = waldorf.GetComponent<Transform>().rotation;
+				break;
+			default: break;
+		}
+	}
+}
---
+++ A2/Assignment2 - Waldorf Project/Assets/Scripts/CameraControllers/SpaceCamera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceCamera : GameCamera
+{
+	public GameObject starting;
+	public Vector3 offset;
+	public GameObject hiding_spot;
+	public float speed;
+	public float minimum_distance;
+	public KeyCode custom;
+	private Vector3 dir;
+
+	private Color color1 = Color.red;
+	private Color color2 = Color.blue;
+	private Color default_color;
+	private readonly float duration = 3.0F;
+
+	// Use this for initialization
+	void Start()
+	{
+		default_color = GetComponent<Camera>().backgroundColor;
+		gameObject.transform.position = starting.transform.position;
+		gameObject.transform.rotation = starting.transform.rotation;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(custom))
+		{
+			Debug.Log(custom + "holding");
+			Debug.Log(dir);
+			if (Vector3.Distance(hiding_spot.GetComponent<Transform>().position, transform.position) > minimum_distance)
+			{
+				dir = Vector3.Normalize(hiding_spot.GetComponent<Transform>().position - transform.position);
+				transform.position = transform.position + dir * speed * Time.deltaTime;
+				float t = Mathf.PingPong(Time.time, duration) / duration;
+				GetComponent<Camera>().backgroundColor = Color.Lerp(color1, color2, t);
+			}//move towards hiding spot at movemet speed;
+		}
+		else
+		{
+			GetComponent<Camera>().backgroundColor = default_color;
+		}
+	}
+}
