bc8b47f9e3307e7d21ca8dc2a21c75aec0be4f2a
mpsslh <mpsslh@hotmail.com>
2018-10-29 20:33:03 +0800

    parsed xml, next creating tiles

---
+++ A4/Assignment #4 - A-maze/Assets/Scripts/MazeBehaviour.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+/// <summary>
+/// The maze behaviour controls the creation and access to the maze
+/// Students must parse data from Xml
+/// Students must then use this parsed data to generate the maze using prefabs
+/// Functions are given for direction and to start the xml process
+/// You are welcome to make any adjustments you need to make this work
+/// </summary>
+/// 
+public class MazeBehaviour : MonoBehaviour
+{
+	[Header("The maze data to generate: switch to new mazes to test!")]
+	public TextAsset mazeData;
+
+	[Header("These properties should NOT change")]
+	[Tooltip("The location to add the tiles")]
+	public GameObject tileContainer;
+	[Tooltip("The location to add objects like Waldorf")]
+	public GameObject objectContainer;
+	[Tooltip("The main camera used for the maze")]
+	public Camera mazeCamera;
+
+	[Tooltip("Prefab: open tile,use in level generation")]
+	public GameObject open0;
+	[Tooltip("Prefab: wall,use in level generation")]
+	public GameObject wall1;
+	// Use this for initialization
+	void Start()
+	{
+		// Let's generate the maze!
+		GenerateMaze();
+	}
+
+	/// <summary>
+	/// Function to generate the maze called at the start of the game
+	/// </summary>
+	private void GenerateMaze()
+	{
+		XmlDocument xml = CreateXmlFromData();
+		CreateTiles(xml);
+		CreateWaldorf(xml);
+		SetCamera(xml);
+	}
+
+	/// <summary>
+	/// Creates the tiles from the xml data
+	/// You will need to make a list of prefabs (0 = open, 1 = wall)
+	/// The maze/tile xml has attributes "xdist" & "zdist" which controls the distance between tiles
+	/// the maze/tile xml has a list of tiles: "#" seperates rows; "," sperates columns
+	/// </summary>
+	/// <param name="xml">The xml document that contains the data</param>
+	private void CreateTiles(XmlDocument xml)
+	{
+		XmlElement maze = (XmlElement)xml.GetElementsByTagName("maze")[0];
+		string tiles = maze.InnerXml;
+		string[] colummn = tiles.Split('#'); //splits strings into columns and stores them in a string array
+	}
+
+	/// <summary>
+	/// Creates a Waldorf character and places him within the maze
+	/// You will need to have a Waldorf prefab
+	/// The maze xml has attributes "spawn" which is in the format [xtile,ztile]
+	/// </summary>
+	/// <param name="xml">The xml document that contains the data</param>
+	private void CreateWaldorf(XmlDocument xml)
+	{
+		//xml, generate and put at location
+	}
+
+	/// <summary>
+	/// Takes the maze camera and places it at an initial position and rotation
+	/// You will need access to the camera in some way
+	/// </summary>
+	/// <param name="xml">The xml document that contains the data</param>
+	private void SetCamera(XmlDocument xml)
+	{
+		XmlElement maze = (XmlElement)xml.GetElementsByTagName("maze")[0];
+		string cam_pos = maze.GetAttributeNode("camPos").InnerXml;
+		string[] p_tokens = cam_pos.Split(','); //splits strings into tokens and stores them in a string array, [0]:x [1]:y [2]:z
+		mazeCamera.GetComponent<Transform>().position = new Vector3(int.Parse(p_tokens[0]), int.Parse(p_tokens[1]), int.Parse(p_tokens[2]));
+		string cam_rot = maze.GetAttributeNode("camRot").InnerXml;
+		string[] rot_tokens = cam_rot.Split(','); //splits strings into tokens and stores them in a string array, [0]:x [1]:y [2]:z
+		mazeCamera.GetComponent<Transform>().rotation = Quaternion.Euler(int.Parse(rot_tokens[0]), int.Parse(rot_tokens[1]), int.Parse(rot_tokens[2]));
+	}
+
+	/// <summary>
+	/// Creates a new xml document from the text asset attached to the behaviour
+	/// </summary>
+	/// <returns>The xml data</returns>
+	private XmlDocument CreateXmlFromData()
+	{
+		XmlDocument xml = new XmlDocument();
+		xml.LoadXml(mazeData.text);
+		return xml;
+	}
+}
---
+++ A4/Assignment #4 - A-maze/Assets/Scripts/WaldorfBehaviour.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Waldorf behaviour controls our hero as he attempts to escape from the maze
+/// Students need to implement four states for Waldorf (Thinking, Moving, Falling, Stuck)
+/// Students need to implement the decision logic for which tile to move to next
+/// Functions are given to handle the movement between tiles
+/// You are welcome to make any adjustments you need to get this working
+/// </summary>
+public class WaldorfBehaviour : MonoBehaviour
+{
+    public const float MIN_DIST = 0.05f; // the minimum distance before changing directions
+
+    [Header("Allows for animations: Do NOT touch!")]
+    public Animator animate;
+
+    [Header("Controls the movement speed and thinking time, adjust as desired")]
+    public float moveSpeed = 1.0f;
+    public float thinkTime = 0.25f;
+
+    // the current tile location Waldorf belongs to
+    // NOTE: this is null when Waldorf is moving as he doesn't belong to any tile
+    private TileBehaviour location = null;
+
+    // the tile location Waldorf is currently moving to
+    // NOTE: this is null when Waldorf is thinking as he doesn't know his next target
+    private TileBehaviour target = null; 
+
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    /// <summary>
+    /// Places Waldorf on a specific tile location
+    /// Should be called when Waldorf is first initialiazed
+    /// </summary>
+    /// <param name="tile">The tile to attach Waldorf to</param>
+    public void SnapToTile(TileBehaviour tile)
+    {
+        this.transform.position = new Vector3(tile.transform.position.x, 0.0f, tile.transform.position.z);
+        location = tile;
+        target = null;
+    }
+
+    /// <summary>
+    /// Assigns a target for Waldorf to move to and faces him in that direction
+    /// Should be called when Waldorf finishes thinking
+    /// </summary>
+    /// <param name="tile">The tile to move Waldorf towards</param>
+    public void MoveToTile(TileBehaviour tile)
+    {
+        location = null;
+        target = tile;
+        this.gameObject.transform.LookAt(new Vector3(tile.transform.position.x, 0.0f, tile.transform.position.z), Vector3.up);
+    }
+
+    /// <summary>
+    /// Moves Waldorf in the direction of the targeted tile and stops him when he reaches it
+    /// Should be called when Waldorf is in the "Move" state
+    /// </summary>
+    private void UpdateMove()
+    {
+        if (target == null) return; // don't call if there is no target
+
+        // Direction to move in
+        Vector3 dir = (target.gameObject.transform.position - this.gameObject.transform.position);
+
+        // Don't move in the y direction
+        dir.y = 0;
+
+        // if direction is less than our min, let's move to the next target
+        if (dir.magnitude <= MIN_DIST)
+        {
+            // set the current target's position, to avoid issues later
+            this.gameObject.transform.position = new Vector3(target.gameObject.transform.position.x, 0.0f, target.gameObject.transform.position.z);
+
+            // assign waldorf to the tile
+            location = target;
+
+            // stop movement by removing the target
+            target = null;
+        }
+
+        // Move Waldorf in the direction by the movement speed
+        this.gameObject.transform.Translate(dir.normalized * moveSpeed * Time.deltaTime, Space.World);
+    }
+}
